4d49721e59af89dcee8fcb4400e4f267f6ecdcd6
Jon Arild Karlsen <karlsen.jonarild@gmail.com>
2018-03-21 15:01:20 +0100

    bug: implement correct url for card display

--- js/app.js
+++ js/app.js
@@ -59,6 +59,14 @@ $(function() {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+// Card event listener
+$(function() {
+    $('.card').click(function(evt) {
+        $(this).children('.front').toggleClass('hide');
+        $(this).children('.back').toggleClass('hide');
+        
+    });
+})
 
 /*
  *
@@ -84,6 +92,6 @@ function shuffle(array) {
 // Create card HTML
 function createCardHTML(array) {
     for (let i = 0; i < cards.length; i++) {
-        cardHTML[i] = `<img/symbols/${array[i]}>`;
+        cardHTML[i] = `<img src="img/symbols/${array[i]}">`;
     }
 }
