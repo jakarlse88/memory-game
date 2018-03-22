5da15a5322b13e1548b8d560e1496d7e4f2533f7
Jon Arild Karlsen <karlsen.jonarild@gmail.com>
2018-03-22 21:29:20 +0100

    refactor: move function declarations above logic

--- js/app.js
+++ js/app.js
@@ -35,85 +35,6 @@ const finishedCards = [];
 
 /*
  *
- * Flags
- * 
- */
-
-let previousCardOpen = false;
-
-/*
- *
- * Logic
- * 
- */
-
-// Display cards to page
-$(function() {
-    // Shuffle "deck"
-    shuffle(cards);
-
-    // Create card HTML
-    createCardHTML(cards);
-
-    // Add each card to page
-    let counter = 0;
-
-    $('.card').each(function() {
-        $(this).children(".front").prepend(cardHTML[counter]);
-        counter++;
-    });
-})
-
-// Card event listener
-$(function() {
-    // Display card symbol
-    $('.card').click(showCard);
-    
-    // Add to list of open cards
-    $('.card').click(addToOpen);
-
-    // If there is already an open card in the list,
-    // check for match
-    $('.card').click(function(e) {
-        if (previousCardOpen) {
-            // Cards match 
-            if ($(openCards)[0].find('img').attr('alt') === $(this).find('img').attr('alt') &&
-                !openCards[0].is($(this))) {
-                console.log(`${$(openCards)[0].find('img').attr('alt')} and ${$(this).find('img').attr('alt')} are a match!`);
-
-                // Add cards to finishedCards
-                addToFinished($(this), openCards[0]);
-
-                // Clear openCards
-                clearOpen();
-                previousCardOpen = false;
-            } 
-            // Cards do not match
-            else {
-                console.log(`card non-match`); // debug
-                console.log($(this));
-
-                hideCard(openCards[0]);
-                hideCard($(this));
-
-                previousCardOpen = false;
-                // setTimeout(function() {
-                //     // Hide cards
-                //     hideCard($(this));
-                //     hideCard(openCards[0]);
-                    
-                //     // Clear openCards
-                //     clearOpen();    
-                // }, 100);
-            }
-        } else {
-            previousCardOpen = true;
-        }
-    });
-})
-
-/*
- *
  * Functions
  * 
  */
@@ -143,11 +64,13 @@ function createCardHTML(array) {
 
 // Display card symbol
 function showCard(e) {
-    if ($(this).children('.front').hasClass('hide') &&
-        !$(this).children('.back').hasClass('hide')) {
-            $(this).children('.front').toggleClass('hide');
-            $(this).children('.back').toggleClass('hide');
-    }
+    $(this).children('.front').toggleClass('hide');
+    $(this).children('.back').toggleClass('hide');
+    // if ($(this).children('.front').hasClass('hide') &&
+    //     !$(this).children('.back').hasClass('hide')) {
+    //         $(this).children('.front').toggleClass('hide');
+    //         $(this).children('.back').toggleClass('hide');
+    // }
 }
 
 // Add card to list of "open" cards
@@ -158,15 +81,15 @@ function addToOpen(e) {
 // Add cards to list of "finished" cards
 function addToFinished(cardOne, cardTwo) {
     let existsInList = false;
-    
+
     for (let i = 0; i < finishedCards.length; i++) {
-        if ($(finishedCards[i]).find('img').attr('alt') === $(cardOne).find('img').attr('alt')||
+        if ($(finishedCards[i]).find('img').attr('alt') === $(cardOne).find('img').attr('alt') ||
             $(finishedCards[i]).find('img').attr('alt') === $(cardTwo).find('img').attr('alt')) {
-            
-                existsInList = true;
+
+            existsInList = true;
         }
     }
-    
+
     if (!existsInList) {
         finishedCards.push(cardOne);
         finishedCards.push(cardTwo);
@@ -176,7 +99,7 @@ function addToFinished(cardOne, cardTwo) {
 // Clear list of "open" cards
 function clearOpen() {
     while (openCards.length >= 1) {
-        openCards.shift();
+        openCards.pop();
     }
 }
 
@@ -185,3 +108,70 @@ function hideCard(card) {
     card.children('.front').toggleClass('hide');
     card.children('.back').toggleClass('hide');
 }
+
+/*
+ *
+ * Logic
+ * 
+ */
+
+// Display cards to page
+$(function () {
+    // Shuffle "deck"
+    shuffle(cards);
+
+    // Create card HTML
+    createCardHTML(cards);
+
+    // Add each card to page
+    let counter = 0;
+
+    $('.card').each(function () {
+        $(this).children(".front").prepend(cardHTML[counter]);
+        counter++;
+    });
+})
+
+// Card event listener
+$(function () {
+    // Display card symbol
+    $('.card').click(showCard);
+
+    // Add to list of open cards
+    $('.card').click(addToOpen);
+
+    // If there is already an open card in the list,
+    // check for match
+    $('.card').click(function (e) {
+        if (openCards.length > 1) {
+            // Cards match 
+            if ($(openCards)[0].find('img').attr('alt') === $(this).find('img').attr('alt') &&
+                !openCards[0].is($(this))) {
+                console.log(`${$(openCards)[0].find('img').attr('alt')} and ${$(this).find('img').attr('alt')} are a match!`);
+
+                // Add cards to finishedCards
+                addToFinished($(this), openCards[0]);
+
+                // Clear openCards
+                clearOpen();
+            }
+            // Cards do not match
+            else {
+                console.log(`card non-match`); // debug
+                console.log($(this));
+
+                hideCard(openCards[0]);
+                hideCard($(this));
+
+                // setTimeout(function() {
+                //     // Hide cards
+                //     hideCard($(this));
+                //     hideCard(openCards[0]);
+
+                //     // Clear openCards
+                //     clearOpen();    
+                // }, 100);
+            }
+        }
+    });
+})
